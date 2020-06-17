5820c708e51c738b848614c31d9c1357c88f463f
Colin James <obscurecolin@gmail.com>
2020-06-17 08:33:35 +0100

    Exclude path from ppx_deriving show (makes AST dump cleaner)

--- src/ast.ml
+++ src/ast.ml
@@ -1,11 +1,11 @@
 
 (* arithmetic operators on integer expressions *)
 type op = Add | Sub | Mul
-[@@deriving show]
+[@@deriving show {with_path = false}]
 
 (* comparison operators on boolean expressions *)
 type cmp = Eq | Ne | Lt | Le | Gt | Ge
-[@@deriving show]
+[@@deriving show {with_path = false}]
 
 (* integer expressions *)
 type expr =
@@ -15,7 +15,7 @@ type expr =
   | Let of string * expr * expr
   | Do of cmd * expr
   | Apply of string * expr list
-[@@deriving show]
+[@@deriving show {with_path = false}]
 (* imperative commands *)
 and cmd =
   | Block of cmd list
@@ -24,22 +24,22 @@ and cmd =
   | Assign of string * expr
   | New of string * expr * cmd
   | Call of string * expr list
-[@@deriving show]
+[@@deriving show {with_path = false}]
 (* boolean expressions *)
 and bexpr =
   | True
   | False
   | Negate of bexpr
   | Bop of cmp * expr * expr
-[@@deriving show]
+[@@deriving show {with_path = false}]
 
 (* return "types" of top-level definitions; int | unit *)
 type typ = Integer | Command
-[@@deriving show]
+[@@deriving show {with_path = false}]
 
 (* imperative constructs *)
 type def =
   | Function of string * string list * expr
   | Routine of string * string list * cmd
-[@@deriving show]
+[@@deriving show {with_path = false}]
 
