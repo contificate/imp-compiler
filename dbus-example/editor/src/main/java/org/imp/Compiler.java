2148327d5ad02c7cd4d67c560b5eed4879623d14
Colin James <obscurecolin@gmail.com>
2020-08-17 22:05:43 +0100

    Remove Java nonsense

--- dbus-example/editor/src/main/java/org/imp/Compiler.java
+++
@@ -1,8 +0,0 @@
-package org.imp;
-
-import org.freedesktop.dbus.interfaces.DBusInterface;
-
-public interface Compiler extends DBusInterface {
-    String CompileIMP(String source);
-    String ParseIMP(String source);
-}
